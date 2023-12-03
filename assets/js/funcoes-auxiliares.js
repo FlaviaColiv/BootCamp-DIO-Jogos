97bfc898e43f4273ad0c6048a9dfa25e2f76a848
Flavia Oliveira <flaviacoliv@gmail.com>
2023-12-03 10:51:34 -0300

    Atualização JS

--- assets/js/funcoes-auxiliares.js
+++ assets/js/funcoes-auxiliares.js
@@ -1,6 +1,10 @@
+const entradas = [5, 50, 10, 98, 23];
+let i = 0;
 
 function gets() {
-  return 10;
+  const valor = entradas[i];
+  i = i + 1;
+  return valor;
 }
 
 function print(texto) {
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,26 @@
-
-
 const { gets, print } = require('./funcoes-auxiliares');
 
-print(gets());
+const numerosSorteados = [];
+
+for (let i = 0; i < 5; i++) {
+  const numeroSorteados = gets();
+  numerosSorteados.push(numeroSorteados); 
+}
+
+print(numerosSorteados);
+
+let maiorValor = 0;
+
+for (let i = 0; i < numerosSorteados.length; i++) {
+  const numeroSorteados = numerosSorteados[i];
+  if (numeroSorteados > maiorValor) {
+    maiorValor = numeroSorteados;
+  }
+}
+
+print(maiorValor);
+
+
+
+
 
