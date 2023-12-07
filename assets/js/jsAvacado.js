81a927e560758826192b721ca39679e3fe6764f6
Flavia Oliveira <flaviacoliv@gmail.com>
2023-12-07 20:12:33 -0300

    JS Atualização

--- assets/js/jsAvacado.js
+++ assets/js/jsAvacado.js
@@ -1,1 +1,23 @@
-console.log("Flavia");
+const pessoa1 = {
+	idade: 34
+}
+
+const flavia1 = {
+	nome: 'Flavia1',
+	__proto__: pessoa1
+}
+
+console.log(flavia1.idade)
+
+/* Criar objetos */
+
+const pessoa2 = {
+	genero: 'feminino'
+}
+
+const flavia2 = Object.create(pessoa2)
+
+flavia2.nome = 'Flavia'
+
+console.log(flavia2.genero)
+
