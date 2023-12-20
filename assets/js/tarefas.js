d2e6590674fb01c80cc555f0592ee80a5df2c4c6
Flavia Oliveira <flaviacoliv@gmail.com>
2023-12-20 02:52:14 -0300

    Anotacoes 2023Dezembro20

--- assets/js/tarefas.js
+++ assets/js/tarefas.js
@@ -1,7 +1,30 @@
 const fs = require('fs')
 
-const leituraArquivo = fs.promises.readFile('tarefas.csv')
+/*
+leituraArquivo
+    .then((arquivo) => arquivo.toString('utf8'))
+    .then((textoDoArquivo) => textoDoArquivo.split('\n').slice(1))
+    .then((linhasSemCabecalho) => linhasSemCabecalho.map((linha) => {
+        const [nome, feito] = linha.split(';')
+        return {
+            nome,
+            feito: feito.trim() === 'true'
+        }
+    }))
+    .then((listaDeTarefas) => console.log(listaDeTarefas))
+    .catch((error) => {console.log('Deu ruim!!!', error)})
+*/
 
-leituraArquivo.then((arquivo) => {
-    console.log(arquivo.toString('utf8'))
-})
+async function buscarArquivo() {
+    try {
+        const arquivo = await fs.promises.readFile('tarefas.csv') /* await tranformado em procedural*/
+        const textoDoArquivo = arquivo.toString('utf8')
+        console.log(textoDoArquivo)
+    } catch (error) {
+        console.log(error)
+    } finally {
+        console.log('Finalizou!')
+    }
+}
+
+buscarArquivo()
